658028e92084137f6d8ef295720c0b9b71bd6935
programmerbanna <programmerbanna@gmail.com>
2024-09-06 02:11:41 +0600

    fourth test add

--- backend/src/app.controller.spec.ts
+++ backend/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Third Test!"', () => {
-      expect(appController.getHello()).toBe('Third Test!');
+    it('should return "Fourth Test!"', () => {
+      expect(appController.getHello()).toBe('Fourth Test!');
     });
   });
 });
--- frontend/src/__tests__/Home.test.jsx
+++ frontend/src/__tests__/Home.test.jsx
@@ -3,10 +3,10 @@ import { render, screen } from "@testing-library/react";
 import Home from "@/app/page";
 
 describe("Home", () => {
-  it("should have Third Test! text", () => {
+  it("should have Fourth Test! text", () => {
     render(<Home />); // Arrange
 
-    const element = screen.getByText("Third Test!"); // Act
+    const element = screen.getByText("Fourth Test!"); // Act
 
     expect(element).toBeInTheDocument(); // Assert
   });
